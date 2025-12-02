a2318c81a4236160f8d1633368cd66eaeeb23012
marumilk <abbydurbridge18@gmail.com>
2025-12-02 17:39:25 +0900

    02 part 2

--- twentyfive/lib/advent_two/advent_two.exs
+++ twentyfive/lib/advent_two/advent_two.exs
@@ -12,8 +12,7 @@ defmodule AdventTwo do
     def solve(input) do
       input
       |> parse_ranges()
-      |> Enum.flat_map(&expand_range/1)
-      |> Enum.filter(&repeated?/1)
+      |> Enum.flat_map(&repeated_numbers_in_range/1)
       |> Enum.sum()
     end
 
@@ -21,24 +20,36 @@ defmodule AdventTwo do
       input
       |> String.trim()
       |> String.split(",", trim: true)
-      |> Enum.map(&parse_range/1)
+      |> Enum.map(fn range_str ->
+        [start, stop] = String.split(range_str, "-")
+        String.to_integer(start)..String.to_integer(stop)
+      end)
     end
 
-    defp parse_range(range_str) do
-      [start, stop] = String.split(range_str, "-")
-      {String.to_integer(start), String.to_integer(stop)}
+    defp repeated_numbers_in_range(range) do
+      range.first
+      |> digit_count()
+      |> Range.new(digit_count(range.last))
+      |> Enum.flat_map(&all_repeated_with_digits/1)
+      |> Enum.uniq()
+      |> Enum.filter(&(&1 in range))
     end
 
-    defp expand_range({start, stop}) do
-      start..stop |> Enum.to_list()
+    defp all_repeated_with_digits(1), do: []
+
+    defp all_repeated_with_digits(digits) do
+      for pattern_len <- 1..div(digits, 2),
+      rem(digits, pattern_len) == 0,
+      pattern <- pattern_range(pattern_len) do
+        pattern
+        |> Integer.to_string()
+        |> String.duplicate(div(digits, pattern_len))
+        |> String.to_integer()
+      end
     end
 
-    defp repeated?(n) do
-      s = Integer.to_string(n)
-      len = String.length(s)
-      half = div(len, 2)
+    defp pattern_range(1), do: 1..9
+    defp pattern_range(len), do: Integer.pow(10, len - 1)..(Integer.pow(10, len) - 1)
 
-      rem(len, 2) == 0 and
-        String.slice(s, 0, half) == String.slice(s, half, half)
-    end
+    defp digit_count(n), do: n |> Integer.to_string() |> String.length()
 end
